c771235df2ee88f35b540694fbc33f284a99d1e5
fieldtheory123 <biyaqiao@hotmail.com>
2023-06-20 18:07:55 +0800

    Add get_vote

---
+++ get_vote.py
@@ -0,0 +1,35 @@
+import requests
+import sys
+
+URL_CURVE = "https://lockers.stakedao.org/api/strategies/cache/curve"
+URL_LOCKER = "https://lockers.stakedao.org/api/lockers/bribes"
+
+def query(url):
+    result = requests.get(url)
+    return result
+
+def find_best_apr_curve():
+    result = query(URL_CURVE)
+    best_apr = 0
+    best_gauge = None
+    for k in result.json():
+        if k['futureWeight'] > 0 and k['apr'] > best_apr:
+            best_apr = k['apr']
+            best_gauge = k['name']
+    return best_apr, best_gauge
+
+def find_best_apr_locker():
+    result = query(URL_LOCKER)
+    best_apr = 0
+    best_gauge = None
+    for k in result.json():
+        if k['futureWeight'] > 0 and k['projectedApr'] > best_apr:
+            best_apr = k['projectedApr']
+            best_gauge = (k['name'], k['address'])
+    return best_apr, best_gauge
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1 or sys.argv[1] == 'curve':
+        print(find_best_apr_curve())
+    elif sys.argv[1] == 'locker':
+        print(find_best_apr_locker())
