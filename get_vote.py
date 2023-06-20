05e35f014aaa7b8abcba91b38ad872a636d8047e
Chase Albert <chase@volume.finance>
2023-06-20 07:55:34 -0600

    Some very opinionated edits.

--- get_vote.py
+++ get_vote.py
@@ -1,35 +1,34 @@
-import requests
+import json
 import sys
+
+import requests
 
 URL_CURVE = "https://lockers.stakedao.org/api/strategies/cache/curve"
 URL_POOL = "https://lockers.stakedao.org/api/lockers/bribes"
 
-def query(url):
-    result = requests.get(url)
-    return result
 
-def find_best_apr_curve():
-    result = query(URL_CURVE)
-    best_apr = 0
-    best_gauge = None
-    for k in result.json():
-        if k['futureWeight'] > 0 and k['apr'] > best_apr:
-            best_apr = k['apr']
-            best_gauge = k['name']
-    return best_apr, best_gauge
+def find_best_apr_curve() -> tuple[float, str]:
+    return max(
+        (k["apr"], k["name"])
+        for k in requests.get(URL_CURVE).json()
+        if k["futureWeight"] > 0
+    )
 
-def find_best_apr_pool():
-    result = query(URL_POOL)
-    best_apr = 0
-    best_gauge = None
-    for k in result.json():
-        if k['futureWeight'] > 0 and k['projectedApr'] > best_apr:
-            best_apr = k['projectedApr']
-            best_gauge = (k['name'], k['address'])
-    return best_apr, best_gauge
 
-if __name__ == '__main__':
-    if len(sys.argv) == 1 or sys.argv[1] == 'curve':
-        print(find_best_apr_curve())
-    elif sys.argv[1] == 'pool':
-        print(find_best_apr_pool())
+def find_best_apr_pool() -> tuple[float, tuple[str, str]]:
+    return max(
+        (k["projectedApr"], (k["name"], k["address"]))
+        for k in requests.get(URL_POOL).json()
+    )
+
+
+if __name__ == "__main__":
+    match sys.argv[1:]:
+        case [] | ["curve"]:
+            apr, gauge = find_best_apr_curve()
+        case ["pool"]:
+            apr, gauge = find_best_apr_pool()
+        case _:
+            print("Call as get_vote.py [curve, pool]")
+            sys.exit(1)
+    json.dump((apr, gauge), sys.stdout)
