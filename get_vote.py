768204738bb2146df7b1c07305e2314b6d47ad00
fieldtheory123 <biyaqiao@hotmail.com>
2023-06-22 21:03:13 +0800

    Add proportional weight functions

--- get_vote.py
+++ get_vote.py
@@ -1,34 +1,62 @@
-import json
-import sys
-
 import requests
+import sys
+import pandas as pd
 
 URL_CURVE = "https://lockers.stakedao.org/api/strategies/cache/curve"
 URL_POOL = "https://lockers.stakedao.org/api/lockers/bribes"
 
+def query(url):
+    result = requests.get(url)
+    return result
 
-def find_best_apr_curve() -> tuple[float, str]:
-    return max(
-        (k["apr"], k["name"])
-        for k in requests.get(URL_CURVE).json()
-        if k["futureWeight"] > 0
-    )
+def find_best_apr_curve():
+    result = query(URL_CURVE)
+    best_apr = 0
+    best_gauge = None
+    for k in result.json():
+        if k['futureWeight'] > 0 and k['apr'] > best_apr:
+            best_apr = k['apr']
+            best_gauge = k['name']
+    return best_apr, best_gauge
 
+def proportional_weight_curve(n_gauges = 5):
+    result = query(URL_CURVE)
+    df = pd.DataFrame()
+    for k in result.json():
+        if k['futureWeight'] > 0:
+            df.loc[k['name'],'weight'] = k['futureWeight']
 
-def find_best_apr_pool() -> tuple[float, tuple[str, str]]:
-    return max(
-        (k["projectedApr"], (k["name"], k["address"]))
-        for k in requests.get(URL_POOL).json()
-    )
+    df = df.sort_values('weight',ascending=False).head(n_gauges)
+    df['weight'] = df['weight'] / df['weight'].sum()
+    return df.to_dict()
 
+def find_best_apr_pool():
+    result = query(URL_POOL)
+    best_apr = 0
+    best_gauge = None
+    for k in result.json():
+        if k['futureWeight'] > 0 and k['projectedApr'] > best_apr:
+            best_apr = k['projectedApr']
+            best_gauge = (k['name'], k['address'])
+    return best_apr, best_gauge
 
-if __name__ == "__main__":
-    match sys.argv[1:]:
-        case [] | ["curve"]:
-            apr, gauge = find_best_apr_curve()
-        case ["pool"]:
-            apr, gauge = find_best_apr_pool()
-        case _:
-            print("Call as get_vote.py [curve, pool]")
-            sys.exit(1)
-    json.dump((apr, gauge), sys.stdout)
+def proportional_weight_pool(n_gauges = 5):
+    result = query(URL_POOL)
+    df = pd.DataFrame()
+    for k in result.json():
+        if k['futureWeight'] > 0:
+            df.loc[k['name'],'weight'] = k['futureWeight']
+
+    df = df.sort_values('weight',ascending=False).head(n_gauges)
+    df['weight'] = df['weight'] / df['weight'].sum()
+    return df.to_dict()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1 or sys.argv[1] == 'curve':
+        print(find_best_apr_curve())
+    elif sys.argv[1] == 'pool':
+        print(find_best_apr_pool())
+    elif sys.argv[1] == 'curve-proportional-weight':
+        print(proportional_weight_curve())
+    elif sys.argv[1] == 'pool-proportional-weight':
+        print(proportional_weight_pool())
